520f2f768d45bb73bbbd3711d7ad6b29173a630d
Adnan abidin TI <31468996+AdnanAbidin@users.noreply.github.com>
2017-09-05 10:46:34 +0700

    Add files via upload

---
+++ Belajar01.java
@@ -0,0 +1,7 @@
+public class Belajar01{
+
+     public static void main(String []args){
+        System.out.println("NAMA : ADNANA ABIDIN");
+        System.out.println("NIM : 17220010");
+     }
+}
